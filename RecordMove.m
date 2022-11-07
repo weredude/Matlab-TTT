bf4ba7b493c79c75394675c60e6b0ea5aeaf91f2
weredude <63823877+weredude@users.noreply.github.com>
2022-11-06 16:23:02 -0800

    Update RecordMove.m

--- RecordMove.m
+++ RecordMove.m
@@ -1,13 +1,13 @@
 %script can access workspace variables
 %each move is added to the winning possiblities, rows, columns, or diagonal
 
-filledRows(moveIndex1) = filledRows(moveIndex1) + currentTurn;
-filledColumns(moveIndex2) = filledColumns(moveIndex2) + currentTurn;
+filledRows(moveIndex1) = filledRows(moveIndex1) + xOrOValue;
+filledColumns(moveIndex2) = filledColumns(moveIndex2) + xOrOValue;
 
 if moveIndex1 == moveIndex2
-    filledDiagonals(1) = filledDiagonals(1) + currentTurn;
+    filledDiagonals(1) = filledDiagonals(1) + xOrOValue;
 end
 if moveIndex1+moveIndex2 == 4
-    filledDiagonals(2) = filledDiagonals(2) + currentTurn;
+    filledDiagonals(2) = filledDiagonals(2) + xOrOValue;
 end
 turnCount = turnCount+1;
